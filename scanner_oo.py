5b6012a274703fee99edcc6179bd8a614efb33c6
Juliusz Wasieleski <tojabonibykto@gmail.com>
2023-10-20 02:55:23 +0200

    my part finished

--- scanner_oo.py
+++ scanner_oo.py
@@ -6,7 +6,7 @@ class Scanner(Lexer):
               DOTADD, DOTSUB, DOTMUL, DOTDIV,
               ASSIGN, ADDASSIGN, SUBASSIGN, MULASSIGN, DIVASSIGN,
               GREATER, LESSER, GEATEREQUAL, LESSEREQUAL, NOTEQUAL, EQUAL,
-              BRACKETOPEN, BRACKETCLOSE, SQBRACKETOPEN, SQBRACKETCLOSE, MSTBRACKETOPEN, MSTBRACKETCLOSE,
+              BRACKETOPEN, BRACKETCLOSE, SQUAREBRACKETOPEN, SQUAREBRACKETCLOSE, CURLYBRACKETOPEN, CURLYBRACKETCLOSE,
               COLON,
               APOSTROPHE,
               COMMA, SEMICOLON}
@@ -36,17 +36,33 @@ class Scanner(Lexer):
     NOTEQUAL = r'!='
     BRACKETOPEN = r'\('
     BRACKETCLOSE = r'\)'
-    SQBRACKETOPEN = r'\['
-    SQBRACKETCLOSE = r'\]'
-    MSTBRACKETOPEN = r'\{'
-    MSTBRACKETCLOSE = r'\}'
+    SQUAREBRACKETOPEN = r'\['
+    SQUAREBRACKETCLOSE = r'\]'
+    CURLYBRACKETOPEN = r'\{'
+    CURLYBRACKETCLOSE = r'\}'
     COLON = r':'
     APOSTROPHE = r'\''
     COMMA = r','
     SEMICOLON = r';'
 
+    ignore_comment = r'\#.*'
+
+    @_(r'\n+')
+    def ignore_newline(self, t):
+        self.lineno += t.value.count('\n')
+
+    def error(self, t):
+        print('Line %d: Bad character %r' % (self.lineno, t.value[0]))
+        # exit(1)
+        self.index += 1
+
+
 if __name__ == '__main__':
-    data = '.+  + ++ -'
+
+    with open("example.txt", "r") as f:
+        data = f.read()
+
     lexer = Scanner()
-    for tok in lexer.tokenize(data):
-        print('type=%r, value=%r' % (tok.type, tok.value))
+    tokenized = lexer.tokenize((data))
+    for tok in tokenized:
+        print(f"({tok.lineno}): {tok.type}({tok.value})")
